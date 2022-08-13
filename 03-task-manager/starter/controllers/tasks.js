e43c3ee265982782a68ca3d622af67daa63b5230
Maciej-Koc <mkoc2@asu.edu>
2022-08-13 16:38:46 -0500

    Deleted files that arent needed

---
+++ 03-task-manager/starter/controllers/tasks.js
@@ -0,0 +1,27 @@
+function getAllTasks(req, res) {
+  res.send("get all tasks");
+}
+
+function createTask(req, res) {
+  res.json(req.body);
+}
+
+function getTask(req, res) {
+  res.json({ id: req.params.id });
+}
+
+function updateTask(req, res) {
+  res.send("update task");
+}
+
+function deleteTask(req, res) {
+  res.send("delete task");
+}
+
+module.exports = {
+  getAllTasks,
+  createTask,
+  getTask,
+  updateTask,
+  deleteTask,
+};
